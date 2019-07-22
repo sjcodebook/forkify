fa002c13d6210a163b475279e7bf27b3de592029
sj <jainsanmati846@gmail.com>
2019-07-22 22:18:23 +0530

    Update recipe servings

--- src/js/index.js
+++ src/js/index.js
@@ -90,3 +90,20 @@ const controlRecipe = async () => {
 };
 
 ['hashchange', 'load'].forEach(e => window.addEventListener(e, controlRecipe));
+
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+  if (e.target.matches('.btn-decrease, .btn-decrease * ')) {
+    // Decrease button is clicked
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings('dec');
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  }
+  if (e.target.matches('.btn-increase, .btn-increase * ')) {
+    // Increase button is clicked
+    state.recipe.updateServings('inc');
+    recipeView.updateServingsIngredients(state.recipe);
+  }
+  console.log(state.recipe);
+});
